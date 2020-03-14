b7acb7fbc98c4c08f07b2ad9874fc059446ae0ea
Dongsik <dongsiku+git@gmail.com>
2020-03-14 14:27:06 +0900

    Add option selector

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,29 +3,47 @@
 SET_UBUNTU_SH_FILENAME=`readlink -f $0`
 SET_UBUNTU_SH_DIRNAME=`dirname $SET_UBUNTU_SH_FILENAME`
 
+MODE=`${$1,,}`
+
+if [ "$MODE" != 'debian-wsl' ] && \
+        [ "$MODE" != 'ubuntu-dualboot' ] && \
+        [ "$MODE" != 'ubuntu-vmware' ]; then
+    echo "Unexpected value: $MODE"
+    exit 1
+fi
+
 # Common 1
 sudo apt update
 packages="git vim python3-venv python3-pip"
 
 # Option
 ## Ubuntu
-### Set ufw
-packages="$packages ufw"
-sudo ufw enable
-sudo ufw default deny
+if [ "$MODE" == 'ubuntu-dualboot' ] || \
+        [ "$MODE" == 'ubuntu-vmware' ]; then
+    ### Set ufw
+    packages="$packages ufw"
+    sudo ufw enable
+    sudo ufw default deny
 
-### edit .bashrc
-cp $HOME/.bashrc $HOME/.bashrc.org
-sed -e 's/#force_color_prompt=yes/force_color_prompt=yes/g' $HOME/.bashrc.org > $HOME/.bashrc
+    ### edit .bashrc
+    cp $HOME/.bashrc $HOME/.bashrc.org
+    sed -e 's/#force_color_prompt=yes/force_color_prompt=yes/g' $HOME/.bashrc.org > $HOME/.bashrc
+fi
 
-### Ubuntu-Native
-sudo timedatectl set-local-rtc true 
+### Ubuntu-Dualboot
+if [ "$MODE" == 'ubuntu-dualboot' ]; then
+    sudo timedatectl set-local-rtc true 
+fi
 
 ### Ubuntu-VMWare
-packages="$packages open-vm-tools open-vm-tools-desktop"
+if [ "$MODE" == 'ubuntu-vmware' ]; then
+    packages="$packages open-vm-tools open-vm-tools-desktop"
+fi
 
 ## WSL
-packages="$packages pdftk"
+if [ "$MODE" == 'debian-wsl' ] || \
+    packages="$packages pdftk"
+fi
 
 # Common 2
 ## Upgrade and Install apt-packages
@@ -44,9 +62,12 @@ git clone https://github.com/dongsiku/MyBashAliases.git
 
 
 # Ubuntu
-## Install git-credential
-sudo apt-get install libgnome-keyring-dev -y
-cd /usr/share/doc/git/contrib/credential/gnome-keyring
-sudo make
-git config --global credential.helper \
-    /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+if [ "$MODE" == 'ubuntu-dualboot' ] || \
+        [ "$MODE" == 'ubuntu-vmware' ]; then
+    ## Install git-credential
+    sudo apt-get install libgnome-keyring-dev -y
+    cd /usr/share/doc/git/contrib/credential/gnome-keyring
+    sudo make
+    git config --global credential.helper \
+        /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+fi
